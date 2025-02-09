5a12399103959cf085e0849e2e1c55ea15c8abd6
Harsh093-k <HarshKashyap701153@gmail.com>
2025-02-09 20:51:43 +0530

    done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require("dotenv").config()
 
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.options('*', cors()); // Handle preflight requests
 
 mongoose.connect(process.env.Mongo_url,{useNewUrlParser: true,
